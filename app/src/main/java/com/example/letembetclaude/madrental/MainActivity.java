e1ce2ac6a6f835f7797f01e69a05d3928e026326
Christian Letembet <claudechristian.letembetambily@ynov.com>
2018-12-06 12:43:45 +0100

    Personalisation Home

--- app/src/main/java/com/example/letembetclaude/madrental/MainActivity.java
+++ app/src/main/java/com/example/letembetclaude/madrental/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.letembetclaude.madrental;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitle;
+    Button btnReservation;
+    Button btnProfil;
+    Button btnRechercher;
+
+    Typeface fontTitle;
+    Typeface fontBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvTitle = (TextView)findViewById(R.id.tvTitle);
+        btnReservation = (Button)findViewById(R.id.btnReservation);
+        btnProfil = (Button)findViewById(R.id.btnProfil);
+        btnRechercher = (Button)findViewById(R.id.btnRechercher);
+
+        fontTitle = Typeface.createFromAsset(getAssets(),"fontTitle.otf");
+        fontBtn = Typeface.createFromAsset(getAssets(),"fontBtn.ttf");
+
+        tvTitle.setTypeface(fontTitle);
+        btnReservation.setTypeface(fontBtn);
+        btnProfil.setTypeface(fontBtn);
+        btnRechercher.setTypeface(fontBtn);
     }
 }
