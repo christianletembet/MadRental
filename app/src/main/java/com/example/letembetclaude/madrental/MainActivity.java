44cd7c916524ace5a0c40f90733735c486127964
Christian Letembet <claudechristian.letembetambily@ynov.com>
2018-12-07 10:40:06 +0100

    Style Home , Profil

--- app/src/main/java/com/example/letembetclaude/madrental/MainActivity.java
+++ app/src/main/java/com/example/letembetclaude/madrental/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.letembetclaude.madrental;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -33,5 +35,14 @@ public class MainActivity extends AppCompatActivity {
         btnReservation.setTypeface(fontBtn);
         btnProfil.setTypeface(fontBtn);
         btnRechercher.setTypeface(fontBtn);
+
+    }
+
+    public void reservations(View view){
+        startActivity(new Intent(this,ReservationActivity.class));
+    }
+
+    public void profil(View view){
+        startActivity(new Intent(this,ProfilActivity.class));
     }
 }
--- app/src/main/java/com/example/letembetclaude/madrental/ProfilActivity.java
+++ app/src/main/java/com/example/letembetclaude/madrental/ProfilActivity.java
@@ -1,13 +1,25 @@
 package com.example.letembetclaude.madrental;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    TextView tvProfil;
+
+    Typeface fontBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        tvProfil = (TextView)findViewById(R.id.tvProfil);
+
+        fontBtn = Typeface.createFromAsset(getAssets(),"fontBtn.ttf");
+
+        tvProfil.setTypeface(fontBtn);
     }
 }
